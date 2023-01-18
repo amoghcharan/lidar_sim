0e495c70de94eeb97b7c252d8ea1b0e074da5a18
Amogh Chinnakonda <achinnakonda3@gatech.edu>
2023-01-17 22:41:16 -0500

    more wurk

--- LiDAR.m
+++ LiDAR.m
@@ -2,7 +2,7 @@
 % lidar range and importable obstacles
 % Amogh Chinnakonda
 
-function intpoints = LIDAR(x1, y1, ang_Res, dist, rect)
+function intpoints = LIDAR(x, N, ang_Res, dist, obs)
 
 % First point generated is at 0 degrees to the right
 % x1 is the current robot's x position
@@ -14,11 +14,6 @@ function intpoints = LIDAR(x1, y1, ang_Res, dist, rect)
 % Number of LiDAR rays
 r = 0:ang_Res:360;
 
-% All endpoints of rays at current position
-x2 = [];
-y2 = [];
-cpos = [x1,y1];
-
 % Outer Vertices of Robotarium
 v1 = [-1.6,-1];
 v2 = [-1.6, 1];
@@ -28,71 +23,132 @@ v4 = [1.6, 1];
 % Empty array of shortest intersection vectors
 intpoints = [];
 
-% Main Ray Generating Algorithm
-for i = 1:length(r)
+% Add dynamic rectangles based on each robot position
+% currrect = {};
+%
+% for c = 1:N
+%     xc = x(1,c)
+%     yc = x(2,c)
+%     ac = x(3,c)
+%     theta = [
+%         ((pi/4) + ac);
+%         ((3*pi/4) + ac);
+%         ((5*pi/4) + ac);
+%         ((7*pi/4) + ac);
+%         ]
+%     for t = 1:4
+%         if theta(t) > 2*pi
+%             theta(t) = theta(t) - (2*pi)
+%         end
+%     end
+%     vc1 = [xc + cos(theta(1)), yc + sin(theta(1))];
+%     vc2 = [xc + cos(theta(2)), yc + sin(theta(2))];
+%     vc3 = [xc + cos(theta(3)), yc + sin(theta(3))];
+%     vc4 = [xc + cos(theta(4)), yc + sin(theta(4))];
+%     currrect = {currrect; [vc1, vc2]; [vc2, vc3]; [vc3, vc4]; [vc4, vc1]}
+%
+% end
 
-    % Current projected end position of ray
-    x2(i) = x1+dist*cosd(r(i));
-    y2(i) = y1+dist*sind(r(i));
-    lpos = [x2(i),y2(i)];
+% Define x,y of each robot
+for h = 1:N
+    x1 = x(1,h);
+    y1 = x(2,h);
 
-    % All obstacle lines
-    r1 = [v1, v2 ; v1, v3; v2, v4; v3, v4; rect(1,1:2), rect(2,1:2); rect(1,1:2), rect(3,1:2); rect(4,1:2), rect(3,1:2); rect(4,1:2), rect(2,1:2)];
-    [numLines,numCols] = size(r1);
+    % All endpoints of rays at current position
+    x2 = [];
+    y2 = [];
+    cpos = [x1,y1];
 
-    % All intersection points on obstacle lines
-    midpoints = [];
+    % Define size of imported obstacles
+    sz = size(obs);
 
-    % Magnitude of intersection vectors
-    norms = [];
+    % Main Ray Generating Algorithm
+    for i = 1:length(r)
 
-    for k = 1:numLines
+        % Current projected end position of ray
+        x2(i) = x1+dist*cosd(r(i));
+        y2(i) = y1+dist*sind(r(i));
+        lpos = [x2(i),y2(i)];
 
-        % Current positions of line vertices
-        v = r1(k,1:4);
-        x3 = v(1);
-        y3 = v(2);
-        x4 = v(3);
-        y4 = v(4);
+        % All obstacle lines
+        vcomb = [v1, v2 ; v1, v3; v2, v4; v3, v4]; %rect(1,1:2), rect(2,1:2); rect(1,1:2), rect(3,1:2); rect(4,1:2), rect(3,1:2); rect(4,1:2), rect(2,1:2)];
 
-        % Line-Line Intersection Parameter Calculations
-        t = ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4))/((x1-x2(i))*(y3-y4) - (y1-y2(i))*(x3-x4));
-        u = ((x1-x3)*(y1-y2(i)) - (y1-y3)*(x1-x2(i)))/((x1-x2(i))*(y3-y4) - (y1-y2(i))*(x3-x4));
+        for m = 1:sz(1)
 
-        % Potential intersection point
-        px = cpos(1) + t*(lpos(1) - cpos(1));
-        py = cpos(2) + t*(lpos(2) - cpos(2));
+            % Checking vertices of each obstacle
+            e = obs{m,:}{1,:};
+            sze = size(e);
+            numv = sze(1);
 
-        % When both parameters are between 0 & 1, intersection is found
-        if (t>=0) && (t<=1) && (u>=0) && (u<=1)
-            midpoints = [midpoints; [px,py]];
+            % Creating first and last connection line segment
+            vcomb = [vcomb; e(1,:) e(end,:)];
+
+            % Adding inner vertice connection line segments
+            for n = 1:(numv-1)
+                vcomb = [vcomb; e(n,:) e(n+1,:)];
+            end
+        end
+
+        [numLines,numCols] = size(vcomb);
+
+        % All intersection points on obstacle lines
+        midpoints = [];
+
+        % Magnitude of intersection vectors
+        norms = [];
+
+        for k = 1:numLines
+
+            % Current positions of line vertices
+            v = vcomb(k,1:4);
+            x3 = v(1);
+            y3 = v(2);
+            x4 = v(3);
+            y4 = v(4);
+
+            % Line-Line Intersection Parameter Calculations
+            t = ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4))/((x1-x2(i))*(y3-y4) - (y1-y2(i))*(x3-x4));
+            u = ((x1-x3)*(y1-y2(i)) - (y1-y3)*(x1-x2(i)))/((x1-x2(i))*(y3-y4) - (y1-y2(i))*(x3-x4));
+
+            % Potential intersection point
+            px = cpos(1) + t*(lpos(1) - cpos(1));
+            py = cpos(2) + t*(lpos(2) - cpos(2));
+
+            % When both parameters are between 0 & 1, intersection is found
+            if (t>=0) && (t<=1) && (u>=0) && (u<=1)
+                midpoints = [midpoints; [px,py]];
+            else
+                midpoints = [midpoints];
+            end
+
+            % Populate Norms with magnitudes of potential endpoints
+            [numR,numC] = size(midpoints);
+            for j = 1:numR
+                norms(j) = [norm([midpoints(j,1)-x1, midpoints(j,2)-y1])];
+            end
+
+            % Find closest intersections in [intpoints]
+            [M,I] = min(norms);
+            intpoint = midpoints(I,:);
+
+        end
+
+        % Append shortest intersection points
+        tf = isempty(intpoint);
+        if tf == 0
+            % [int_x, int_y, int_dist]
+            intpoints = [intpoints; [intpoint(1), intpoint(2), M]];
         else
-            midpoints = [midpoints];
+            intpoints = [intpoints];
         end
 
-        % Populate Norms with magnitudes of potential endpoints
-        [numR,numC] = size(midpoints);
-        for j = 1:numR
-            norms(j) = [norm([midpoints(j,1)-x1, midpoints(j,2)-y1])];
-        end
 
-        % Find closest intersections in [intpoints]
-        [M,I] = min(norms);
-        intpoint = midpoints(I,:);
+        % Add Gausian Distortion to points
+    %     for l = 1:length(intpoints(:,1))
+    %         intang = [
 
     end
-
-    % Append shortest intersection points
-    tf = isempty(intpoint);
-    if tf == 0
-        intpoints = [intpoints; [intpoint(1), intpoint(2)]];
-    else
-        intpoints = [intpoints];
-    end
-
-    % Add Gausian Distortion to points
-%     for l = 1:length(intpoints(:,1))
-%         intang = [
 
 end
+
 
---
+++ go_to_pose.m
@@ -0,0 +1,96 @@
+% Initializing the agents to random positions with barrier certificates.
+% This script shows how to initialize robots to a particular pose.
+% Paul Glotfelter edited by Sean Wilson
+% Modified for LiDAR function integration
+% 07/2019
+
+N = 3;
+initial_positions = generate_initial_conditions(N, 'Spacing', 0.5);
+r = Robotarium('NumberOfRobots', N, 'ShowFigure', true, 'InitialConditions', initial_positions);
+
+% Set some parameters for use with the barrier certificates.  We don't want
+% our agents to collide
+
+% Create a barrier certificate for use with the above parameters
+unicycle_barrier_certificate = create_uni_barrier_certificate_with_boundary();
+
+%Get randomized initial conditions in the robotarium arena
+final_goal_points = generate_initial_conditions(N, ...
+    'Width', r.boundaries(2)-r.boundaries(1)-r.robot_diameter, ...
+    'Height', r.boundaries(4)-r.boundaries(3)-r.robot_diameter, ...
+    'Spacing', 0.5);
+
+args = {'PositionError', 0.025, 'RotationError', 0.05};
+init_checker = create_is_initialized(args{:});
+controller = create_waypoint_controller(args{:});
+
+% Get initial location data for while loop condition.
+x=r.get_poses();
+r.step();
+
+% Sample Rectangle
+% lx = -1;
+% rx = -0.3;
+% ly = -0.2;
+% ry = 0.5;
+% r2 = rectangle('Position', [lx,ly, abs(rx-lx), abs(ry-ly)],'EdgeColor', 'r',...
+%                'LineWidth', 2);
+
+% Define Obstacles by sequential vertice coordinates
+r1 = [-1, -1];
+r2 = [-1, 0];
+r3 = [0, -1];
+
+l1 = [1.1, 0.8];
+l2 = [0.6, 0.8];
+l3 = [0.6, 0.5];
+l4 = [0.5, 0.3];
+
+% Plotting Setup for shapes
+line_width = 3;
+
+lr1 = line([r1(1), r2(1)], [r1(2), r2(2)], 'LineWidth', line_width, 'Color', 'k')
+lr2 = line([r2(1), r3(1)], [r2(2), r3(2)], 'LineWidth', line_width, 'Color', 'k')
+lr3 = line([r1(1), r3(1)], [r1(2), r3(2)], 'LineWidth', line_width, 'Color', 'k')
+
+ll1 = line([l1(1), l2(1)], [l1(2), l2(2)], 'LineWidth', line_width, 'Color', 'k')
+ll2 = line([l2(1), l3(1)], [l2(2), l3(2)], 'LineWidth', line_width, 'Color', 'k')
+ll3 = line([l3(1), l4(1)], [l3(2), l4(2)], 'LineWidth', line_width, 'Color', 'k')
+ll4 = line([l1(1), l4(1)], [l1(2), l4(2)], 'LineWidth', line_width, 'Color', 'k')
+
+% Structure vertices as cell array
+obs = {
+    {[r1; r2; r3]};
+    {[l1; l2; l3; l4]}
+    };
+
+% Plotting Initialization
+% Choose color of points in RGB values
+RGB = [0 190 119]/256;
+scan = [0,0];
+h = scatter(scan(:,1), scan(:,2), 144, RGB, 'filled');
+
+while(~init_checker(x, final_goal_points))
+    x = r.get_poses();
+    dxu = controller(x, final_goal_points);
+    dxu = unicycle_barrier_certificate(dxu, x);
+
+    % Call LIDAR to find all plottable intersection points
+    scan = LIDAR(x, N, 36, 0.5, obs);
+
+    % Plot intersection points
+    [numP,numCols] = size(scan);
+
+    for g = 1:numP
+        % Update LiDAR scatter
+        set(h,'XData',scan(:,1),'YData',scan(:,2));
+    end
+
+    r.set_velocities(1:N, dxu);
+    r.step();
+end
+
+% We can call this function to debug our experiment!  Fix all the errors
+% before submitting to maximize the chance that your experiment runs
+% successfully.
+r.debug();
